dea6dd8d477545384e68ac49355dfbbee4be3212
Mavounzy <montoutmiguel@gmail.com>
2023-12-07 09:42:30 +0100

    modif

--- asset/js/main.js
+++ asset/js/main.js
@@ -2,7 +2,7 @@ const icons = document.querySelector('#icons')
 const links = document.querySelectorAll("nav li")
 const nav = document.querySelector('#nav_burger')
 
-icons.addEventListener('click', (evt) =>{
+icons.addEventListener('click', (evt) => {
     evt.preventDefault()
     nav.classList.toggle("active")
 })
